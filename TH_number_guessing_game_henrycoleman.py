fbd6390e60f6953c268e83b54a7bd409e2ee782f
colemanhenry <46647019+colemanhenry@users.noreply.github.com>
2019-04-07 11:14:02 +0100

    Add files via upload

--- TH_number_guessing_game_henrycoleman.py
+++ TH_number_guessing_game_henrycoleman.py
@@ -10,47 +10,46 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 
 import random
 
-
 def start_game():
-
+    
     hi_score = [] # Stores the player score
-    
-    
+    continue_game = "y"
     print("=" * 48 +  "\nWelcome to the Super Cool Number Guessing Game!\n" + "=" * 48 + "\n"  )
     name = input("What is your name? ")
     print("Hello {}, best of luck & lets begin! ".format(name))
     
-    continue_game = "y" # Gives player the option to end the game
-    player_guess = None
-    player_attempts = 0
-    answer = random.randint(1,10)
-            
+    while continue_game == "y":
+        player_guess = None
+        player_attempts = 0
+        answer = random.randint(1,10)    
     
-    while player_guess != answer:
-        try:
-            player_guess = int(input("Please guess a number between 1 & 10: "))
-            while (player_guess < 1) or (player_guess > 10):
-                player_guess = int(input(" Whoops! Rule Breaker! You must enter a number between 1 & 10! "))
+        while player_guess != answer:
+            try:
+                player_guess = int(input("Please guess a number between 1 & 10: "))
+                while (player_guess < 1) or (player_guess > 10):
+                    player_guess = int(input(" Whoops! Rule Breaker! You must enter a number between 1 & 10! "))
 
-            player_attempts += 1
-            if player_guess > answer:
-                print("<===Guess lower")
-            elif player_guess < answer:
-                print("===>Guess higher")
+                player_attempts += 1
+                if player_guess > answer:
+                    print("<===Guess lower")
+                elif player_guess < answer:
+                    print("===>Guess higher")
 
-            else:
-                print("Winner! Fantastic Stuff! It took you {} attempt(s). You Champion!".format(player_attempts))
-                hi_score.append(player_attempts)
-                continue_game = input(" Would you like to play again? y or n ?: ")
-                if continue_game.lower() == "n":
-                    print("Bye for now & see you soon!")
                 else:
-                    print("The High score is {}".format(min(hi_score)))
+                    print("Winner! Fantastic Stuff! It took you {} attempt(s). You Champion!".format(player_attempts))
+                    hi_score.append(player_attempts)
+                    continue_game = input("Would you like to play again? y or n ?: ")
+                    if continue_game.lower() == "n":
+                        print("=" * 27 +  "\nBye for now & see you soon!\n" + "=" * 27 + "\n"  )
+                    else:
+                        print("The High score is {}".format(min(hi_score)))
             
-        except SyntaxError as err:
-            print("Invalid value ! ({})\n Please try again". format(err))
-        except NameError as err:
-            print("Invalid value ! ({})\n Please try again". format(err))
+            except SyntaxError as err:
+                print("Invalid value ! ({})\n Please try again". format(err))
+            except NameError as err:
+                print("Invalid value ! ({})\n Please try again". format(err))
+            except ValueError as err:
+                print("Invalid value ! ({})\n Please try again". format(err))
 
 if __name__=='__main__':
     start_game()
